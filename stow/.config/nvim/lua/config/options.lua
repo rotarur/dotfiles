37011cb4024336509b5b1cdc3e2e63651acbdc81
Ruslan Rotaru <rotarur.social.apps@gmail.com>
2025-11-21 19:14:59 +0000

    chore [nvim]: add few options for swap and undo and viminfo/shada

--- stow/.config/nvim/lua/config/options.lua
+++ stow/.config/nvim/lua/config/options.lua
@@ -1,9 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- local opt = vim.opt
+local opt = vim.opt
 -- local g = vim.g
 local filetype = vim.filetype
 
+-- Save more info in viminfo/shada file
+opt.shada = "'1000,<50,s10,h" -- Remember more files, lines, etc.
+opt.undolevels = 1000
+opt.undoreload = 10000
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
 -- opt.ligatures = false
 -- g.filetype = "on"
