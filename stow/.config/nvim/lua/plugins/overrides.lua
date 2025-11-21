f909f8d5da83af4e13e8727c6a786b9dccb1d5a4
Ruslan Rotaru <rotarur.social.apps@gmail.com>
2025-11-21 19:17:01 +0000

    chore [nvim]: add overrides for autocmd for restore cursor

--- stow/.config/nvim/lua/plugins/overrides.lua
+++ stow/.config/nvim/lua/plugins/overrides.lua
@@ -5,15 +5,14 @@ return {
       enable_bracket_in_quote = true,
     },
   },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      spec = {
-        ["<leader>C"] = { name = "+Claude Code" },
-        ["<leader>r"] = { name = "+Rename" },
-      },
-    },
-  },
+  -- {
+  --   "folke/which-key.nvim",
+  --   opts = {
+  --     spec = {
+  --       ["<leader>r"] = { name = "+Rename" },
+  --     },
+  --   },
+  -- },
   {
     "nvim-telescope/telescope.nvim",
     opts = {
@@ -28,5 +27,27 @@ return {
         },
       },
     },
+  },
+  {
+    "LazyVim",
+    opts = {},
+    init = function()
+      vim.api.nvim_create_autocmd("BufReadPost", {
+        group = vim.api.nvim_create_augroup("RestoreCursor", { clear = true }),
+        callback = function(event)
+          local exclude_ft = { "gitcommit", "gitrebase", "svn", "hgcommit" }
+          local buf = event.buf
+          if vim.tbl_contains(exclude_ft, vim.bo[buf].filetype) or vim.b[buf].last_loc then
+            return
+          end
+          vim.b[buf].last_loc = true
+          local mark = vim.api.nvim_buf_get_mark(buf, '"')
+          local lcount = vim.api.nvim_buf_line_count(buf)
+          if mark[1] > 0 and mark[1] <= lcount then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+          end
+        end,
+      })
+    end,
   },
 }
