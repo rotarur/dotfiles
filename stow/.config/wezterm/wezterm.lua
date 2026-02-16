e5c06460aca3e4744d02e1cd158371fa5a8b5883
Ruslan Rotaru <rotarur.social.apps@gmail.com>
2026-02-16 18:16:20 +0000

    fix wezterm: change font name

--- stow/.config/wezterm/wezterm.lua
+++ stow/.config/wezterm/wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font = wezterm.font("FiraCode Retina")
+
 config.font_size = 18
 
 config.color_scheme = "s3r0 modified (terminal.sexy)"
